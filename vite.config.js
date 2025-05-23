b9bcc1633dd163fccf2124a0a0043d40bdd769c5
Boxy <nozakisola@gmail.com>
2025-05-22 20:58:57 -0500

    Debian Stuffz

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from 'path'
+
+export default {
+  root: resolve(__dirname, 'src'),
+  build: {
+    outDir: '../dist'
+  },
+  server: {
+    port: 8080
+  },
+  preview: {
+    port: 8080
+  },
+  plugins: []
+}
